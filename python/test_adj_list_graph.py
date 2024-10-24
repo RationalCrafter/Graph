83acf09a3cd666f9d1e480e61bd449d547c2d424
pejalb@live.com <pejalb@live.com>
2024-10-24 11:35:48 -0300

    Create small test suite for AdjacencyListGraph.

---
+++ python/test_adj_list_graph.py
@@ -0,0 +1,59 @@
+import pytest
+from adj_list_graph import AdjacencyListGraph
+
+
+# test basic vertex insertion
+def test_10_vertex_insertion():
+    g = AdjacencyListGraph()
+    for i in range(10):
+        g.add_vertex(i)
+    # assert existence
+    for i in range(10):
+        # doesn't directly require overloading
+        assert g.has_vertex(i) is True
+        # uses __contains__
+        assert i in g
+
+
+# test basic vertex insertion with large vertex set
+def test_10000_vertex_insertion():
+    g = AdjacencyListGraph()
+    for i in range(10000):
+        g.add_vertex(i)
+    # assert existence
+    for i in range(10000):
+        assert i in g
+
+
+# test basic edge creation
+def test_small_edge_set():
+    g = AdjacencyListGraph()
+    g.add_vertex("a")
+    g.add_vertex("b")
+    g.add_vertex("c")
+    # add edges
+    g.add_edge("a", "b")
+    g.add_edge("b", "c")
+    # test edges
+    assert g.has_edge("a", "b")
+    assert g.has_edge("b", "c")
+    assert not g.has_edge("a", "c")
+
+
+# test graph creation with fully connected small graph
+def test_fully_connected_10v_creation():
+    g = AdjacencyListGraph()
+    # add vertices
+    for i in range(10):
+        g.add_vertex(i)
+    # assert vertex existence
+    for i in range(10):
+        assert g.has_vertex(i)
+    # add edges
+    for i in range(10):
+        for j in range(10):
+            g.add_edge(i, j)
+    # test edge existence
+    for i in range(10):
+        for j in range(10):
+            assert g.has_edge(i, j) is True
