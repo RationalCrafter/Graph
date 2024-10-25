ad399eb0cfe701b4ad59ec137d30aff9acc4f530
pejalb@live.com <pejalb@live.com>
2024-10-25 16:58:24 -0300

    Implement DFS.

---
+++ python/dfs.py
@@ -0,0 +1,28 @@
+from simple_graph import Graph
+
+
+def dfs(graph: Graph):
+    """Depth-first search implementation for Graph types"""
+    color = {u: "white" for u in graph}
+    parent = {u: None for u in graph}
+    discovery = {u: None for u in graph}
+    finalization = {u: None for u in graph}
+    time = 0
+
+    def dfs_visit(graph: Graph, u, time):
+        time += 1
+        discovery[u] = time
+        color[u] = "gray"
+        for v in graph.get_neighbors(u):
+            if color[v] == "white":
+                parent[v] = u
+                dfs_visit(graph, v, time)
+        color[u] = "black"
+        time += 1
+        finalization[u] = time
+
+    for u in graph:
+        if color[u] == "white":
+            dfs_visit(graph, u, time)
+
+    return parent, discovery, finalization
